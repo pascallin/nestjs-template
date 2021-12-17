85fce3a9b5e728aef8981fd50f8c46227b6c226c
pascallin <pascal_lin@foxmail.com>
2021-12-17 18:13:46 +0800

    chore: export middlewares

--- src/app/middlewares/index.ts
+++ src/app/middlewares/index.ts
@@ -3,3 +3,4 @@ export * from './requestId.interceptor';
 export * from './requestLog.interceptor';
 export * from './validation.pipe';
 export * from './response.interceptor';
+export * from './objectId.pipe';
